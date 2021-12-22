d6619cd1bfe711bbd6915cb8413fe2811ffb1d06
D_W13 <wang.y.derek@gmail.com>
2021-12-22 00:56:00 -0500

    fixed algo for filter calculations

--- filters/linear_filter.py
+++ filters/linear_filter.py
@@ -7,8 +7,15 @@ IMG = 'flower.jpg'
 img = cv.imread(IMG)
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-mask = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]])
+mask = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+# mask = np.array([[0.0625, 0.125, 0.0625], [0.125, 0.25, 0.125], [0.0625, 0.125, 0.0625]])
+
+def get_new_intensity(m1, m2):
+    total = 0
+    for i in range(len(m1)):
+        total += np.dot(m1[i], m2[i]).sum()
+    return total
+
 
 result = []
 for i in range(1, len(img) - 1):
@@ -20,13 +27,14 @@ for i in range(1, len(img) - 1):
         bottom = [img[i + 1][j - 1], img[i + 1][j], img[i + 1][j + 1]]
         square = np.array([top, mid, bottom])
 
-        mapped_ops = np.dot(np.subtract(square, mask), mask)
-        print(mapped_ops.sum())
-        new.append(mapped_ops.sum())
+        # mapped_ops = np.dot(np.subtract(square, mask), mask)
+        # print(mapped_ops.sum())
+        new.append(get_new_intensity(square, mask))
 
     result.append(new)
 
 print(np.uint8(result))
 cv.imshow('test', np.array(np.uint8(result)))
+cv.imshow('original', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
